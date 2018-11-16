294468483024bc4c83e0803aedbdb6412f072133
tanhe123 <xiayule148@gmail.com>
2018-11-16 12:55:43 +0800

    fix travis

--- test.sh
+++ test.sh
@@ -8,14 +8,18 @@ IMAGE=${IMAGE:?image missing}
 if [[ "$RUNTIME" = "java8" ]]; then 
     MOUNT=$(pwd)/demos/$RUNTIME/target
     HANDLER=examples.Hello::handleRequest
+    INITIALIZER=examples.Hello::initialize
 else 
     MOUNT=$(pwd)/demos/$RUNTIME 
     HANDLER=index.handler
+    INITIALIZER=initializer
 fi 
 
 echo "mount: $MOUNT"
 
-if ! docker run --rm -it -v $MOUNT:/code $IMAGE $HANDLER | grep -q '2' ; then
+echo "docker run --rm -v $MOUNT:/code $IMAGE -h $HANDLER -i $INITIALIZER"
+
+if ! docker run --rm -it -v $MOUNT:/code $IMAGE -h $HANDLER -i $INITIALIZER | grep -q '2' ; then
     echo "runtime $RUNTIME test failed"; 
     exit 1; 
 fi 
