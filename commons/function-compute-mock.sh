90e9027f87b54c75bf958b9fdd878e492469b87b
Hehe Tan <443016215@qq.com>
2018-12-29 11:22:08 +0800

    add stdin and http mode (#17)
    * fix fc-docker system version
    
    * add stdin mode and http mode and refactor code
    
    * fix runtime permission denied when using nas

--- commons/function-compute-mock.sh
+++ commons/function-compute-mock.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-set -e
-
 SHELL_DIR="$(dirname $0)"
 event="{}"
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-handler="${1:-index.handler}"
+handler="index.handler"
 
 timeout=3
 initializer=
@@ -21,6 +19,8 @@ while true; do
     -e | --initializationTimeout ) initializationTimeout="$2"; shift 2 ;;
     --timeout ) timeout="$2"; shift 2 ;;
     --event ) event="$2"; shift 2 ;;
+    --stdin ) STDIN=true; shift ;; # use stdin as event
+    --http ) HTTP_MODE=true; shift ;; 
     -- ) shift; break ;;
     "" ) break ;;
     * ) echo -e "\n\t Please use the long and short parameter mode. \n\t For more details, please refer to https://github.com/aliyun/fc-docker. \n\n"; exit -1 ;;
@@ -55,9 +55,10 @@ done
 
 startTimestamp="$(date '+%s')$(date '+%N')"
 
-if [ -n "$initializer" ]; then
-    curl -s -X POST localhost:${serverPort}/initialize \
+curlUtil() {
+    curl -s -X POST $3 localhost:${serverPort}/$1 \
         -H "Content-Type: application/octet-stream" \
+        -H "Expect: " \
         -H "x-fc-request-id: $requestId" \
         -H "x-fc-function-name: ${FC_FUNCTION_NAME:-fc-docker}" \
         -H "x-fc-function-memory: ${memoryLimit}" \
@@ -67,22 +68,33 @@ if [ -n "$initializer" ]; then
         -H "x-fc-function-handler: ${handler}" \
         -H "x-fc-access-key-id: ${FC_ACCESS_KEY_ID}" \
         -H "x-fc-access-key-secret: ${FC_ACCESS_KEY_SECRET}" \
-        -H "x-fc-security-token: ${FC_SECURITY_TOKEN}"
+        -H "x-fc-security-token: ${FC_SECURITY_TOKEN}" \
+        -H "${HTTP_PARAMS_HEADER}" \
+        --data-binary "$2"
+}
+
+if [ -n "$initializer" ]; then
+    curlUtil initialize ""
 fi
 
-curl -s -X POST localhost:${serverPort}/invoke \
-    -H "Content-Type: application/octet-stream" \
-    -H "x-fc-request-id: $requestId" \
-    -H "x-fc-function-name: ${FC_FUNCTION_NAME:-fc-docker}" \
-    -H "x-fc-function-memory: ${memoryLimit}" \
-    -H "x-fc-function-timeout: ${timeout}" \
-    -H "x-fc-function-handler: ${handler}" \
-    -H "x-fc-initialization-timeout: ${initializationTimeout}" \
-    -H "x-fc-function-initializer: ${initializer}" \
-    -H "x-fc-access-key-id: ${FC_ACCESS_KEY_ID}" \
-    -H "x-fc-access-key-secret: ${FC_ACCESS_KEY_SECRET}" \
-    -H "x-fc-security-token: ${FC_SECURITY_TOKEN}" \
-    --data-binary "$event"
+HTTP_PARAMS_HEADER="${FC_HTTP_PARAMS:+x-fc-http-params: $FC_HTTP_PARAMS}"
+
+# use stdin as event
+if [ -n "$STDIN" ]; then 
+    # display http response headers and body
+    if [ -n "$HTTP_MODE" ]; then
+        RESPONSE=$(curlUtil invoke @- '-i')
+    else 
+        curlUtil invoke @-
+    fi
+else 
+    if [ -n "$HTTP_MODE" ]; then
+        RESPONSE=$(curlUtil invoke $event '-i')
+    else 
+        curlUtil invoke "$event"
+    fi
+fi
+
 
 endTimestamp="$(date '+%s')$(date '+%N')"
 memoryUsage=$[$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes) / 1024 / 1024]
@@ -90,6 +102,16 @@ memoryUsage=$[$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes) / 1024 / 1024]
 billedTime=$[(endTimestamp - startTimestamp) / 1000000]
 
 echo -e "\n\n${GREEN}RequestId: ${requestId} \t Billed Duration: ${billedTime} ms \t Memory Size: ${memoryLimit} MB \t Max Memory Used: ${memoryUsage} MB${NC}\n"
+
+if [ -n "$HTTP_MODE" ]; then
+    echo "--------------------response begin-----------------"
+    echo "$RESPONSE" | base64
+    echo "--------------------response end-----------------"
+
+    echo "--------------------execution info begin-----------------"
+    echo -e "${requestId}\n${billedTime}\n${memoryLimit}\n${memoryUsage}" | base64
+    echo "--------------------execution info end-----------------"
+fi
 
 # kill all child process before exit.
 # pkill -SIGINT -P "$(jobs -p)"
