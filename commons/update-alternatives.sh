84be3a4c7e3175a5ef89d058dfdc4f3bddc4909d
coco-super <sunfeiyu_java@163.com>
2019-07-03 17:15:03 +0800

    modify shell

--- commons/update-alternatives.sh
+++ commons/update-alternatives.sh
@@ -3,18 +3,18 @@ array=($*)
 install="--install"
 slave="--slave"
 prefix="/code/.fun/root"
-if [ !$FUN_INSTALL_LOCAL ] || [ $FUN_INSTALL_LOCAL = "false" ]; then
 
-    update-alternatives-origin $*
-    
-elif [ $FUN_INSTALL_LOCAL] && [ $FUN_INSTALL_LOCAL = "true" ]; then
-
+if [ "$FUN_INSTALL_LOCAL" = "true" ]; then
     for ((i = 0; i < ${#array[@]}; i++)); do
         if [[ ${array[i]} == $install || ${array[i]} == $slave ]]; then
             array[i + 1]=$prefix${array[i + 1]}
             array[i + 3]=$prefix${array[i + 3]}
         fi
     done
+    mkdir -p /code/.fun/root/usr/bin
+    mkdir -p /code/.fun/root/etc/alternatives
 
-    update-alternatives-origin ${array[@]} --altdir /code/.fun/root/etc/alternatives/
+    update-alternatives-origin ${array[@]} --altdir /code/.fun/root/etc/alternatives
+else
+    update-alternatives-origin $*
 fi
