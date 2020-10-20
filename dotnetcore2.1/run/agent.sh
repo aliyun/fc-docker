f2dc6f27676a410bd8715b50d79432f125163fbc
rsonghuster <ls_huster@163.com>
2020-10-20 18:16:36 +0800

    fix all fc-docker runtime image

--- dotnetcore2.1/run/agent.sh
+++ dotnetcore2.1/run/agent.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 CURDIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
+# Set some default configuration.
+export FC_SERVER_PORT=${FC_SERVER_PORT:=9000}
+export FC_SERVER_PATH=${FC_SERVER_PATH:=${CURDIR}}
+export FC_SERVER_LOG_PATH=${FC_SERVER_LOG_PATH:=${CURDIR}/var/log}
+export FC_SERVER_LOG_LEVEL=${FC_SERVER_LOG_LEVEL:=INFO}
+
+export FC_FUNC_CODE_PATH=${FC_FUNC_CODE_PATH:=${CURDIR}}
+export FC_FUNC_LOG_PATH=${FC_FUNC_LOG_PATH:=${CURDIR}/var/log}
+
 serverPort=${FC_SERVER_PORT:-9000}
 codePath=${FC_FUNC_CODE_PATH:-${CURDIR}}
 /usr/bin/dotnet /var/fc/runtime/dotnetcore2.1/bootstrap/Aliyun.Serverless.CA.dll -port ${serverPort} -codepath ${codePath}
---
+++ php7.2/run/agent.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+###################################################################
+# To start the server, use this script.
+#
+# The server is configured with environment variables. The full list
+# of environment variables can be seen in ./src/constant.js
+###################################################################
+
+# The absolute path of this file.
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+# Set some default configuration.
+export FC_SERVER_PORT=${FC_SERVER_PORT:=9000}
+export FC_SERVER_PATH=${FC_SERVER_PATH:=${CURDIR}}
+export FC_SERVER_LOG_PATH=${FC_SERVER_LOG_PATH:=${CURDIR}/var/log}
+export FC_SERVER_LOG_LEVEL=${FC_SERVER_LOG_LEVEL:=INFO}
+
+export FC_FUNC_CODE_PATH=${FC_FUNC_CODE_PATH:=${CURDIR}}
+export FC_FUNC_LOG_PATH=${FC_FUNC_LOG_PATH:=${CURDIR}/var/log}
+
+Help() {
+    echo "Usage:"
+    echo "      $0 command"
+    echo "Commands"
+    echo "      start               start up server"
+    echo "      help                print help page"
+}
+
+
+if [ $# -eq 0 ]
+then
+    echo "Missing arguments."
+    Help
+    exit 0;
+fi
+
+command=$1
+case ${command} in
+    "start")
+        php  ${FC_SERVER_PATH}/src/server.php
+        ;;
+    "help")
+        Help
+        ;;
+    *)
+        Help
+        ;;
+esac
